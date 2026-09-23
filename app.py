05dab36cdeb7b0075fb0cbb64a0408ba857905f7
Infin8tyX <devp18@NYCSTUDENTS.NET>
2026-09-23 09:26:14 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,19 +11,22 @@ input(99.99)
 amt = 100
 #Float uses decimals
 amt_two = 99.99
-servicefeedback = input("How was your experience?")
-input("good")
+servicefeedback = print("How was your experience?")
+servicefeedback = input("good")
 if servicefeedback == "good":
         tipoptions = input("Thank you!, do you want to tip?")
         input("yes")
         if tipoptions == "yes":
+                x = 1.10
+                y = 1.20
                 print("Okay, do you want to tip 10 or 20 percent?")
                 input("10%")
-input("bad")
+        else: print("man screw you") 
+else: input("bad")
 if servicefeedback == "bad":
         print("What was wrong with your experience?")
         input("too expensive")
-        print("Thanks for your feedback")
+        print("I hate you dude, i work 80 hours every week just for you to say you had a bad experience")
 #BooLean
-x = True
-y = False
+#x = True
+#y = False
