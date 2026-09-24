607b1a27b889bab4d81211932295b65b914e8789
Infin8tyX <devp18@NYCSTUDENTS.NET>
2026-09-24 09:20:49 -0400

    Update app.py

--- app.py
+++ app.py
@@ -8,21 +8,28 @@ bill = input("How much was the bill")
 print(bill)
 #integer uses whole numbers
 input(99.99)
-amt = 100
 #Float uses decimals
-amt_two = 99.99
+amt = 99.99
 servicefeedback = print("How was your experience?")
+servicefeedback = input("amazing")
+if servicefeedback == input("amazing"):
+        tipoptions = print("Wow thank you! Would you likem to leave a tip of 25%?")
+        input("of course")
+        if tipoptions: input("of course")
+        bill = (amt*1.25)
+        else: print("DUDE YOU HAD AN AMAZING EXPERIENCE JUST TIP")
+        input("nah")
 servicefeedback = input("good")
 if servicefeedback == "good":
         tipoptions = input("Thank you!, do you want to tip?")
         input("yes")
         if tipoptions == "yes":
                 x = 1.10
-                y = 1.20
-                print("Okay, do you want to tip 10 or 20 percent?")
-                input("10%")
+                print("Okay, do you want to tip 10%?")
+                input("yes")
+                bill = (amt*10)
         else: print("man screw you") 
-else: input("bad")
+servicefeedback = input("bad")
 if servicefeedback == "bad":
         print("What was wrong with your experience?")
         input("too expensive")
